30d683e0f2bed3529d8237dad71e6fcbd4ad57dd
MonjulH <monjulhazarika4@gmail.com>
2026-04-05 12:54:07 +0530

    Modification for analyzing guquarter data

--- sound_analysis.R
+++ sound_analysis.R
@@ -1,48 +1,5 @@
-#First script for sound analysis
-
-library(seewave)
-library(tuneR)
-library(soundecology)
-#import audio for analysis:
-audio1 <- readWave("D:/M.Sc/4th Sem AEWB/Dissertation/sound1_06label.wav")
-
-#Acoustic Complexity index
-aci <- acoustic_complexity(audio1) #execute for dataset
-  ##average values taken for calculation
-    aci_avg<-mean(c(aci$AciTotAll_left,aci$AciTotAll_right))
-    aci_bymin_avg<-mean(c(aci$AciTotAll_left_bymin,aci$AciTotAll_right_bymin))
-
-#Bioacoustic index
-bi <- bioacoustic_index(audio1) #execute for dataset
-  ##average values taken for calculation:
-    bi_avg<-mean(c(bi$left_area,bi$right_area))
-
-#Normalized Difference Soundscape Index
-ndsi_value <- ndsi(audio1) #execute for dataset
-  ##average values taken for calculation:
-    ndsi_avg<-mean(c(ndsi_value$ndsi_left,ndsi_value$ndsi_right))
-    antph_avg<-mean(c(ndsi_value$anthrophony_left,ndsi_value$anthrophony_right))
-    bioph_avg<-mean(c(ndsi_value$biophony_left,ndsi_value$biophony_right))
-  
-# Combine the averages into a single table
-    sound_results <- data.frame(
-      File_Name = "sound1_06label.wav",
-      ACI_avg = aci_avg,
-      ACI_bymin_avg = aci_bymin_avg,
-      BI_avg = bi_avg,
-      NDSI_avg = ndsi_avg,
-      Anthrophony_avg = antph_avg,
-      Biophony_avg = bioph_avg
-    )
-    
-View(sound_results)
-
-#This script is executed for multiple labels from a single recording
-#Multiple labels from the single location would then be analyzed to calculate average indices values of a single sample site
-
-#Finalized code for acoustic indices analysis
-
-#code2#-----------
+#Finalized script for sound analysis from each site
+#code2#----
 
 library(seewave)
 library(tuneR)
@@ -93,6 +50,7 @@ for (rep_name in names(replicates)) {
   #Callback for specific replicate
   current_files <- replicates[[rep_name]]
   
+  #inner loop with analysis for each replicate
   for (f in current_files) {
     message(paste("Processing file:", basename(f)))
     
@@ -129,5 +87,10 @@ View(master_sound_results)
 #Create csv for further analysis
 library(tidyverse)
 write.csv(master_sound_results, file = "botanicalgarden.csv", row.names = FALSE)
+
+
+# Analyses of indices from all sites together # ----
+
+
 
 
